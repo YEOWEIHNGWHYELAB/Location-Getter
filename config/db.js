7a98cc8cd61d2cb72b046ad76bee8e7e301cf07f
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2023-04-18 15:21:11 +0800

    refactoring and create table automatically if not exist

--- config/db.js
+++ config/db.js
@@ -1,4 +1,12 @@
 const { Pool } = require("pg");
+const fs = require('fs');
+const path = require('path');
+
+// Get the absolute path to the SQL script file
+const sqlScriptPath = path.join(__dirname, 'createTable.sql');
+
+// Read the SQL script file
+const sqlScript = fs.readFileSync(sqlScriptPath).toString();
 
 const pool = new Pool({
   user: process.env.DATABASE_USER.toString(),
@@ -8,12 +16,26 @@ const pool = new Pool({
   port: process.env.DATABASE_PORT.toString(),
 });
 
-pool.connect((err, client, done) => {
-  if (err) 
-    throw err;
-  
-  console.log(`Connected to database: ${process.env.DATABASE_NAME.toString()}`);
-});
+pool.connect()
+  .then(() => {
+    // Execute the SQL script
+    pool.query(sqlScript)
+      .then(() => {
+        console.log('SQL initializer script executed successfully');
+      })
+      .catch((err) => {
+        console.error('Error executing SQL script', err);
+      })
+      .finally(() => {
+        // Close the database connection
+        pool.end();
+      });
+
+    console.log(`Connected to database: ${process.env.DATABASE_NAME.toString()}`);
+  })
+  .catch((err) => {
+    console.error('Error connecting to PostgreSQL database', err);
+  });
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
